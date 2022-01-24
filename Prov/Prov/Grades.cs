d68e58609262609335a1d54b22de363e0f46ff12
Philip <philip.bernstrom@elev.ga.ntig.se>
2022-01-24 09:18:48 +0100

    Initial commit

---
+++ Prov/Prov/Grades.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace prov
+{
+    public class Grades
+    {
+        public int gradeA = 10;
+
+        public int gradeC = 5;
+
+        public int gradeE = 1;
+
+        public int gradeF = 0;
+
+    }
+}
---
+++ Prov/Prov/Heroes.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace prov
+{
+    public class Heroes
+    {
+        public int health = 100;
+
+        public int powers;
+
+        public int gradepoints;
+
+        public int name;
+
+
+
+    }
+}
